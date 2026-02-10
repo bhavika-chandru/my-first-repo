522c8753c885c581a3b40467dc385c6224372bd7
bhavika-chandru <bhavikachandru8@gmail.com>
2026-02-10 09:58:52 +0100

    Add install script

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ sudo apt update
 sudo apt install -y curl
 echo "Installation complete!"
 
+#!/usr/bin/env bash
+echo "Starting installation..."
+sudo apt update
+sudo apt install -y curl
+echo "Installation complete!"
+
